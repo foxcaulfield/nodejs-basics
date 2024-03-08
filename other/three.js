faf0870d901b21463c35c0a1ce21c8b3749b3cf3
foxcaulfield <fox.caulfield@gmail.com>
2024-03-08 21:48:57 +0100

    Delete unused files

--- other/three.js
+++
@@ -1,8 +0,0 @@
-// function printWord() {
-//     const words = ["Madness", "What", "Is"];
-//     const index = Math.floor(Math.random() * words.length);
-//     console.log(words[index]);
-// }
-
-// // export const pW = printWord;
-// exports.pW = printWord;
