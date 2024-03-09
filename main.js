2602b58c9fff8b82f581acac723c7672883889fc
foxcaulfield <fox.caulfield@gmail.com>
2024-03-09 17:52:25 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-const hero = require("./super-hero");
-
-console.log(hero.name);
-hero.name = "SuperMan";
-console.log(hero.name);
-
-const hero2 = require("./super-hero");
-console.log(hero2.name);
+// require("./examples/path_ref");
+// console.log("test2");
+// console.log(getRandomWord());
