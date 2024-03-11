bae294b3eeb438e9b15fa526701284b15c7d61b9
foxcaulfield <fox.caulfield@gmail.com>
2024-03-11 09:19:45 +0100

    Update main.ts file

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,6 @@
 // console.log("test2");
 // console.log(getRandomWord());
 
-require("./examples/Pizza");
+// require("./examples/Pizza");
+// require("./examples/buffer_ref");
+require("./examples/fs_ref");
