995b9e2249a0fa78cc24cb250c2c214e859f8863
foxcaulfield <fox.caulfield@gmail.com>
2024-03-14 11:30:38 +0100

    Update crypto_+ https example

--- src/examples/crypto_ref.ts
+++ src/examples/crypto_ref.ts
@@ -1,20 +1,30 @@
-import crypto from "node:crypto";
+// import crypto from "node:crypto";
 import os from "node:os";
+import https from "node:https";
 
 // const start = Date.now();
 // crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
 // console.log(Date.now() - start);
 
 // @ts-ignore
-process.env.UV_THREADPOOL_SIZE = 16;
+// process.env.UV_THREADPOOL_SIZE = 16;
 
 console.log(os.cpus().length);
-const MAX_CALLS = 100;
+const MAX_CALLS = 50;
 
 const start = Date.now();
 for (let i = 0; i < MAX_CALLS; ++i) {
-  crypto.pbkdf2("password", "salt", 500000, 512, "sha512", (_err, _derived) => {
-    // console.log(derived);
-    console.log(`Hash: ${i + 1}`, Date.now() - start);
-  });
+  https.request("https://google.com/", (res) => {
+    res.on("data", (_chunk) => {
+      // console.log("chunk", chunk);
+    });
+    res.on("end", () => {
+      console.log(`Request: ${i + 1}`, Date.now() - start);
+    });
+  }).end();
+
+  // crypto.pbkdf2("password", "salt", 500000, 512, "sha512", (_err, _derived) => {
+  //   // console.log(derived);
+  //   console.log(`Hash: ${i + 1}`, Date.now() - start);
+  // });
 }
