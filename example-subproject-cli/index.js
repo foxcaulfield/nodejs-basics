f52d300161e4ba51a4d1f6f294221ea0f6b4e7e8
foxcaulfield <fox.caulfield@gmail.com>
2024-03-15 16:20:33 +0100

    Update subproject for experiments with CLI tools

--- example-subproject-cli/index.js
+++ example-subproject-cli/index.js
@@ -2,12 +2,20 @@
 
 const yargs = require("yargs");
 
-console.log("Done!");
-
 const y = yargs(process.argv);
+// console.log(y.argv);
 
-console.log(process.argv);
-console.log(process.argv[0]);
-console.log(process.argv0);
-console.log(y);
-console.log(y.argv);
+const printPokemonMoves = async (pokemonName) => {
+    const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    const data = await result.json();
+    const moves = data.moves.map(move => move.move.name);
+    console.log(moves);
+}
+
+printPokemonMoves(y.argv.pokemon);
+
+// console.log(process.argv);
+// console.log(process.argv[0]);
+// console.log(process.argv0);
+// console.log(y);
+// console.log(y.argv);
